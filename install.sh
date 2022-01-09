2781dfc0d1da884ebd5ec088b520d696ef854d13
XOMUS <93247740+XOMUS@users.noreply.github.com>
2022-01-09 20:05:13 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mkdir /home/$(whoami)/.tlauncher/
-cp ~/Tl-legacy-installer/icon.png /home/$(whoami)/.tlauncher/
-cp ~/Tl-legacy-installer/appicon.desktop /home/$(whoami)/.local/share/applications/
-cp ~/Tl-legacy-installer/TL.jar /home/$(whoami)/.tlauncher/
+cp icon.png /home/$(whoami)/.tlauncher/
+cp appicon.desktop /home/$(whoami)/.local/share/applications/
+cp TL.jar /home/$(whoami)/.tlauncher/
 sudo pacman -S openjdk11-src
 sudo apt-get update
 sudo apt-get install openjdk-11-jdk-headless
