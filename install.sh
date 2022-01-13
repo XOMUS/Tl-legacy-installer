4e11232a964b6c1e36e1ad71860513fe0a38da57
XOMUS <93247740+XOMUS@users.noreply.github.com>
2022-01-13 21:54:17 +0500

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-mkdir /home/$(whoami)/.tlauncher/
-cp icon.png /home/$(whoami)/.tlauncher/
-cp appicon.desktop /home/$(whoami)/.local/share/applications/
-cp TL.jar /home/$(whoami)/.tlauncher/
-sudo pacman -S openjdk11-src
-sudo apt-get update
-sudo apt-get install openjdk-11-jdk-headless
-sudo yum install java-1.11.0-openjdk
-sudo dnf install java-1.11.0-openjdk
